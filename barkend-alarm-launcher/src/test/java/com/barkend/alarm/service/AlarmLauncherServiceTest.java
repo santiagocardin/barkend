9cf7768203c7b5fb9c92dd4c9798df9caa7e1a06
Santiago Cardin <santiago.cardin@gmail.com>
2021-08-23 21:41:22 +0200

    use testcontainers for kafka tests

--- barkend-alarm-launcher/src/test/java/com/barkend/alarm/service/AlarmLauncherServiceTest.java
+++ barkend-alarm-launcher/src/test/java/com/barkend/alarm/service/AlarmLauncherServiceTest.java
@@ -5,6 +5,12 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.KafkaContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
 
 import java.time.LocalTime;
 
@@ -13,9 +19,18 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.verify;
 
 @SpringBootTest
+@Testcontainers
 @AutoConfigureWireMock(port = 0)
 @ActiveProfiles("test")
 class AlarmLauncherServiceTest {
+
+	@Container
+	private static KafkaContainer kafka = new KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka:6.2.0"));
+
+	@DynamicPropertySource
+	static void setup(DynamicPropertyRegistry registry) {
+		registry.add("spring.kafka.bootstrap-servers", kafka::getBootstrapServers);
+	}
 
 	@SpyBean
 	AlarmLauncherService alarmLauncherService;
